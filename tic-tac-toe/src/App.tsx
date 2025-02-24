4935154336d4b20a1d3882772748f270cd2a23f5
jstrohsnitter <jstrohsnitter@gmail.com>
2025-02-24 18:35:16 -0500

    intial comit, board built

---
+++ tic-tac-toe/src/App.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+/*-------------------------------- Constants --------------------------------*/
+
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+  return (
+    <>
+      <div className='game'>
+        <h1 className='title'>Tic-Tac-Toe</h1>
+        <h2 className='message'>Message</h2>
+
+        <section className="board">
+        <div className="sqr" id="0"></div>
+        <div className="sqr" id="1"></div>
+        <div className="sqr" id="2"></div>
+        <div className="sqr" id="3"></div>
+        <div className="sqr" id="4"></div>
+        <div className="sqr" id="5"></div>
+        <div className="sqr" id="6"></div>
+        <div className="sqr" id="7"></div>
+        <div className="sqr" id="8"></div>
+        </section>
+      </div>
+
+    </>
+  )
+}
+
+export default App
